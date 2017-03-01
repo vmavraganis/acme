dfaeee7d6cb56f2fa633e0351d5a1188615aa315
vmavraganis <vlmavraganis@hotmail.com>
2017-03-01 19:23:07 +0200

    Update index.js

--- config/index.js
+++ config/index.js
@@ -6,6 +6,6 @@ module.exports={
 getDbConnectionString :  function() {
                 //mongodb://localhost:27017/integration_test
         
-       return `mongodb://${configValues.username}:${configValues.password}@localhost:27017/${configValues.db}`;;
+       return `mongodb://${configValues.username}:${configValues.password}@localhost:${configValues.port}/${configValues.db}`;;
 }
 }
